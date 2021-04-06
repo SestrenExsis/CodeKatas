c7fbb44116584e26926939fa5677e5e5638a2023
Joshua Carrigan <sestren@comcast.net>
2021-04-05 19:25:08 -0500

    Solve Advent of Code 2015 Day 1

--- adventofcode/AdventOfCode2015.py
+++ adventofcode/AdventOfCode2015.py
@@ -28,35 +28,6 @@ def get_raw_input_lines() -> list:
             break
     return raw_input_lines
 
-class Day01: # Not Quite Lisp
-    '''
-    Not Quite Lisp
-    https://adventofcode.com/2015/day/1
-    '''
-    def get_parsed_input(self, raw_input_lines: List[str]):
-        result = []
-        for raw_input_line in raw_input_lines:
-            result.append(raw_input_line)
-        return result
-    
-    def solve(self, parsed_input):
-        result = len(parsed_input)
-        return result
-    
-    def solve2(self, parsed_input):
-        result = len(parsed_input)
-        return result
-    
-    def main(self):
-        raw_input_lines = get_raw_input_lines()
-        parsed_input = self.get_parsed_input(raw_input_lines)
-        solutions = (
-            self.solve(parsed_input),
-            self.solve2(parsed_input),
-            )
-        result = solutions
-        return result
-
 class Template: # Template
     '''
     Template
@@ -86,6 +57,48 @@ class Template: # Template
         result = solutions
         return result
 
+class Day01: # Not Quite Lisp
+    '''
+    Not Quite Lisp
+    https://adventofcode.com/2015/day/1
+    '''
+    def get_parsed_input(self, raw_input_lines: List[str]):
+        result = raw_input_lines[0]
+        return result
+    
+    def solve(self, parsed_input):
+        floor = 0
+        for char in parsed_input:
+            if char == '(':
+                floor += 1
+            elif char == ')':
+                floor -= 1
+        result = floor
+        return result
+    
+    def solve2(self, parsed_input):
+        floor = 0
+        position = 0
+        for position, char in enumerate(parsed_input, start=1):
+            if char == '(':
+                floor += 1
+            elif char == ')':
+                floor -= 1
+            if floor == -1:
+                result = position
+                break
+        return result
+    
+    def main(self):
+        raw_input_lines = get_raw_input_lines()
+        parsed_input = self.get_parsed_input(raw_input_lines)
+        solutions = (
+            self.solve(parsed_input),
+            self.solve2(parsed_input),
+            )
+        result = solutions
+        return result
+
 if __name__ == '__main__':
     '''
     Usage
@@ -93,30 +106,30 @@ if __name__ == '__main__':
     '''
     solvers = {
         1: (Day01, 'Not Quite Lisp'),
-    #     2: (Day02, 'Inventory Management System'),
-    #     3: (Day03, 'No Matter How You Slice It'),
-    #     4: (Day04, 'Repose Record'),
-    #     5: (Day05, 'Alchemical Reduction'),
-    #     6: (Day06, 'Chronal Coordinates'),
-    #     7: (Day07, 'The Sum of Its Parts'),
-    #     8: (Day08, 'Memory Maneuver'),
-    #     9: (Day09, 'Marble Mania'),
-    #    10: (Day10, 'The Stars Align'),
-    #    11: (Day11, 'Chronal Charge'),
-    #    12: (Day12, 'Subterranean Sustainability'),
-    #    13: (Day13, 'Mine Cart Madness'),
-    #    14: (Day14, 'Chocolate Charts'),
-    #    15: (Day15, 'Beverage Bandits'),
-    #    16: (Day16, 'Chronal Classification'),
-    #    17: (Day17, 'Reservoir Research'),
-    #    18: (Day18, 'Settlers of The North Pole'),
-    #    19: (Day19, 'Go With The Flow'),
-    #    20: (Day20, 'A Regular Map'),
-    #    21: (Day21, 'Chronal Conversion'),
-    #    22: (Day22, 'Mode Maze'),
-    #    23: (Day23, 'Experimental Emergency Teleportation'),
-    #    24: (Day24, 'Immune System Simulator 20XX'),
-    #    25: (Day25, 'Four-Dimensional Adventure'),
+    #     2: (Day02, '???'),
+    #     3: (Day03, '???'),
+    #     4: (Day04, '???'),
+    #     5: (Day05, '???'),
+    #     6: (Day06, '???'),
+    #     7: (Day07, '???'),
+    #     8: (Day08, '???'),
+    #     9: (Day09, '???'),
+    #    10: (Day10, '???'),
+    #    11: (Day11, '???'),
+    #    12: (Day12, '???'),
+    #    13: (Day13, '???'),
+    #    14: (Day14, '???'),
+    #    15: (Day15, '???'),
+    #    16: (Day16, '???'),
+    #    17: (Day17, '???'),
+    #    18: (Day18, '???'),
+    #    19: (Day19, '???'),
+    #    20: (Day20, '???'),
+    #    21: (Day21, '???'),
+    #    22: (Day22, '???'),
+    #    23: (Day23, '???'),
+    #    24: (Day24, '???'),
+    #    25: (Day25, '???'),
         }
     parser = argparse.ArgumentParser()
     parser.add_argument('day', help='Solve for a given day', type=int)
