57455378ba5d3de2996048bec1e4cdd0af08bc51
Joshua Carrigan <sestren@comcast.net>
2021-04-09 18:50:12 -0500

    Refactor CheatingDetection problem a bit

--- withgoogle/GoogleCodeJam2021.py
+++ withgoogle/GoogleCodeJam2021.py
@@ -276,7 +276,7 @@ class CheatingDetection:
     player_count = 100
     question_count = 10_000
 
-    def solve(self, player_answers: list):
+    def solve(self, player_answers: list, partition_threshold: float):
         difficulties = [0] * self.question_count
         for player_id, answers in enumerate(player_answers):
             for question_id, answer in enumerate(answers):
@@ -284,34 +284,37 @@ class CheatingDetection:
                     difficulties[question_id] += 1
         # Partition the questions into two groups: easy and hard
         questions = []
-        for question_id, difficulty in enumerate(difficulties):
-            heapq.heappush(questions, (-difficulty, question_id))
-        hard_questions = []
-        for _ in range(self.question_count // 2):
-            _, question_id = heapq.heappop(questions)
-            hard_questions.append(question_id)
+        max_partition_size = int(partition_threshold * self.question_count)
         easy_questions = []
-        for _, question_id in questions:
-            easy_questions.append(question_id)
+        hard_questions = []
+        for question_id, difficulty in enumerate(difficulties):
+            if len(easy_questions) > max_partition_size:
+                heapq.heappushpop(easy_questions, (-difficulty, question_id))
+            else:
+                heapq.heappush(easy_questions, (-difficulty, question_id))
+            if len(hard_questions) > max_partition_size:
+                heapq.heappushpop(hard_questions, (difficulty, question_id))
+            else:
+                heapq.heappush(hard_questions, (difficulty, question_id))
         quiz = []
         for player_id in range(self.player_count):
             easy_questions_correct = 0
-            for question_id in easy_questions:
+            for _, question_id in easy_questions:
                 if player_answers[player_id][question_id] == '1':
                     easy_questions_correct += 1
             hard_questions_correct = 0
-            for question_id in hard_questions:
+            for _, question_id in hard_questions:
                 if player_answers[player_id][question_id] == '1':
                     hard_questions_correct += 1
             quiz.append((easy_questions_correct, hard_questions_correct))
         cheater = None
-        lowest_ratio = float('inf')
+        lowest_diff = float('inf')
         for player_id in range(self.player_count):
             easy_questions_correct, hard_questions_correct = quiz[player_id]
-            ratio = easy_questions_correct / hard_questions_correct
-            if ratio < lowest_ratio:
+            diff = easy_questions_correct - hard_questions_correct
+            if diff < lowest_diff:
                 cheater = player_id
-                lowest_ratio = ratio
+                lowest_diff = diff
         result = cheater + 1
         return result
     
@@ -323,7 +326,7 @@ class CheatingDetection:
             player_answers = []
             for i in range(self.player_count):
                 player_answers.append(input())
-            solution = self.solve(player_answers)
+            solution = self.solve(player_answers, 0.10)
             output_row = 'Case #{}: {}'.format(
                 test_id,
                 solution,
