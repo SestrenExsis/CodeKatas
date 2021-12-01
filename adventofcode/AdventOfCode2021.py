3bfea84cdc8a99505daf3ae6fa46b572e5d4cd48
Joshua Carrigan <sestren@comcast.net>
2021-11-30 23:37:54 -0600

    Solve 2021 Day 1

--- adventofcode/AdventOfCode2021.py
+++ adventofcode/AdventOfCode2021.py
@@ -52,30 +52,42 @@ class Template: # Template
         result = solutions
         return result
 
-class Day01: # XXX
+class Day01: # Sonar Sweep
     '''
     https://adventofcode.com/2021/day/1
     '''
-    def get_parsed_input(self, raw_input_lines: List[str]):
-        result = []
+    def get_sonar_sweeps(self, raw_input_lines: List[str]):
+        sonar_sweeps = []
         for raw_input_line in raw_input_lines:
-            result.append(raw_input_line)
+            sonar_sweeps.append(int(raw_input_line))
+        result = sonar_sweeps
         return result
     
-    def solve(self, parsed_input):
-        result = len(parsed_input)
+    def solve(self, sonar_sweeps):
+        count = 0
+        for i in range(1, len(sonar_sweeps)):
+            if sonar_sweeps[i] > sonar_sweeps[i - 1]:
+                count += 1
+        result = count
         return result
     
-    def solve2(self, parsed_input):
-        result = len(parsed_input)
+    def solve2(self, sonar_sweeps):
+        count = 0
+        a, b, c = sonar_sweeps[0], sonar_sweeps[1], sonar_sweeps[2]
+        for i in range(3, len(sonar_sweeps)):
+            d = sonar_sweeps[i]
+            if d > a:
+                count += 1
+            a, b, c = b, c, d
+        result = count
         return result
     
     def main(self):
         raw_input_lines = get_raw_input_lines()
-        parsed_input = self.get_parsed_input(raw_input_lines)
+        sonar_sweeps = self.get_sonar_sweeps(raw_input_lines)
         solutions = (
-            self.solve(parsed_input),
-            self.solve2(parsed_input),
+            self.solve(sonar_sweeps),
+            self.solve2(sonar_sweeps),
             )
         result = solutions
         return result
@@ -86,7 +98,7 @@ if __name__ == '__main__':
     python AdventOfCode2021.py 1 < inputs/2021day01.in
     '''
     solvers = {
-        1: (Day01, 'XXX'),
+        1: (Day01, 'Sonar Sweep'),
     #     2: (Day02, 'XXX'),
     #     3: (Day03, 'XXX'),
     #     4: (Day04, 'XXX'),
