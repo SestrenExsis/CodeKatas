56400d6bec108ee44bbfcee0c9d863cc396386ce
Joshua Carrigan <sestren@comcast.net>
2021-12-05 23:35:15 -0600

    Solve Elevator Saga Challenges 2-4

--- elevatorsaga/elevatorsaga.js
+++ elevatorsaga/elevatorsaga.js
@@ -1,14 +1,15 @@
 
 {
     init: function(elevators, floors) {
-        var elevator = elevators[0]; // Let's use the first elevator
-        elevator.on("idle", function() {
-            elevator.goToFloor(0);
-        });
-
-        elevator.on("floor_button_pressed", function(floorNum) {
-            elevator.goToFloor(floorNum)
-        });
+        for (const elevator of elevators) {
+            elevator.on("idle", function() {
+                elevator.goToFloor(0);
+            });
+    
+            elevator.on("floor_button_pressed", function(floorNum) {
+                elevator.goToFloor(floorNum)
+            });
+        }
     },
     
     update: function(dt, elevators, floors) {
