d982512edfa18dc558b066f4af616fcc5e64f59d
Joshua Carrigan <sestren@comcast.net>
2022-04-23 12:30:15 -0500

    Solve Equal Sum

--- withgoogle/solvers/EqualSum.py
+++ withgoogle/solvers/EqualSum.py
@@ -1,4 +1,13 @@
+import logging
 import sys
+
+logging.basicConfig(
+    filename = 'InteractiveProblem.out',
+    filemode ='w',
+    level = logging.DEBUG,
+    format = '%(asctime)s - %(levelname)s: %(message)s',
+    datefmt = '%m/%d/%Y %I:%M:%S %p',
+    )
 
 '''
 Smallest 100 numbers total to 5,050
@@ -6,10 +15,14 @@ Largest 100 numbers total to 100,999,994,950
 Largest power of two allowed is 2 ^ 29 or 536,870,912
 '''
 
+logging.debug('EqualSum')
+
 T = int(input())
 for i in range(T):
-    _ = int(input())
-    N = 100 # Assume N is always 100
+    logging.debug(f'Test #{i + 1}')
+    N = int(input())
+    assert N == 100
+    logging.debug(f'  N={N}')
     # Generate valid powers of two
     powers = set()
     for exp in range(30):
@@ -30,9 +43,13 @@ for i in range(T):
     message = ' '.join(map(str, pool))
     print(message)
     sys.stdout.flush()
+    logging.debug(f'  message={message}')
     # receive response
-    response = tuple(map(int, input().split(' ')))
-    pool.union(response)
+    response = input()
+    logging.debug(f'  response={response}')
+    judge_pool = tuple(map(int, response.split(' ')))
+    pool = pool.union(judge_pool)
+    logging.debug(f'  total_sum={sum(pool)}')
     left_nums = []
     right_nums = []
     while len(pool) > len(powers):
@@ -40,9 +57,25 @@ for i in range(T):
         pool.remove(next_num)
         left_sum = sum(left_nums)
         right_sum = sum(right_nums)
-        left_diff = abs(left_sum + next_num - right_sum)
-        right_diff = abs(right_sum + next_num - left_sum)
-        if left_diff <= right_diff:
+        if left_sum < right_sum:
             left_nums.append(next_num)
-    target = pool // 2
+        else:
+            right_nums.append(next_num)
+    while len(pool) > 0:
+        next_num = max(num for num in pool)
+        pool.remove(next_num)
+        left_sum = sum(left_nums)
+        right_sum = sum(right_nums)
+        if left_sum < right_sum:
+            left_nums.append(next_num)
+        else:
+            right_nums.append(next_num)
+    left_sum = sum(left_nums)
+    right_sum = sum(right_nums)
+    logging.debug(f'  left_sum={left_sum}')
+    logging.debug(f'  right_sum={right_sum}')
+    # assert sum(left_nums) == sum(right_nums)
+    message = ' '.join(map(str, left_nums))
+    print(message)
+    sys.stdout.flush()
     
