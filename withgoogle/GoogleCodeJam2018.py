830f116c23a12869b2d3410a7fc0e49d501b91d4
Joshua Carrigan <sestren@comcast.net>
2023-11-28 05:53:14 -0600

    Tidy up a bit

--- withgoogle/GoogleCodeJam2018.py
+++ withgoogle/GoogleCodeJam2018.py
@@ -102,6 +102,62 @@ class TroubleSort: # Q.2
             print(output_row)
         return output
 
+class WaffleChoppers: # 1A.1
+    def solve(self, waffle: list, h_cuts: int, v_cuts: int):
+        rows = len(waffle)
+        cols = len(waffle[0])
+        total_chip_count = 0
+        row_chips = [0] * rows
+        column_chips = [0] * cols
+        for row in range(rows):
+            chip_count = 0
+            for col in range(cols):
+                if waffle[row][col] == '@':
+                    column_chips[col] += 1
+                    chip_count += 1
+            row_chips[row] = chip_count
+            total_chip_count += chip_count
+        result = True
+        if total_chip_count > 0:
+            diner_count = (h_cuts + 1) * (v_cuts + 1)
+            chips_per_piece = int(chip_count // diner_count)
+            if total_chip_count != chips_per_piece * diner_count:
+                result = False
+            else:
+                chips_per_row = chips_per_piece * (v_cuts + 1)
+                chips_per_col = chips_per_piece * (h_cuts + 1)
+                h_partitions = [0]
+                # Check rows
+                row_chip_count = 0
+                for row in range(rows):
+                    row_chip_count += row_chips[row]
+                    if row_chip_count == chips_per_row:
+                        row_chip_count = 0
+                        h_partitions.append(row + 1)
+                if len(h_partitions) - 2 != h_cuts:
+                    result = False
+                # Check columns
+        return result
+    
+    def main(self):
+        test_count = int(input())
+        output = []
+        for test_id in range(1, test_count + 1):
+            rows, cols, h_cuts, v_cuts = tuple(map(int, input().split(' ')))
+            waffle = []
+            for _ in range(rows):
+                waffle.append(input())
+            solution = 'IMPOSSIBLE'
+            if self.solve(waffle, h_cuts, v_cuts):
+                solution = 'POSSIBLE'
+            output_row = 'Case #{}: {}'.format(
+                test_id,
+                solution,
+                )
+            output.append(output_row)
+            print(output_row)
+        return output
+
 # Template for Submission page
 '''
 import collections
@@ -152,11 +208,12 @@ class Template:
 if __name__ == '__main__':
     '''
     Usage
-    python GoogleCodeJam2018.py Q.2 < inputs/TroubleSort.in
+    python GoogleCodeJam2018.py 1A.1 < inputs/WaffleChoppers.in
     '''
     solvers = {
         'Q.1': (SavingTheUniverseAgain, 'Saving The Universe Again'),
         'Q.2': (TroubleSort, 'Trouble Sort'),
+        '1A.1': (WaffleChoppers, 'Waffle Choppers'),
         # 'Solver': (Solver, 'Solver'),
         }
     parser = argparse.ArgumentParser()
