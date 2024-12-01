864cef107a108f591852b9f1728b300b17023446
SestrenExsis <sestrenexsis@gmail.com>
2024-11-30 21:49:56 -0600

    Create template for Advent of Code 2024

---
+++ adventofcode/AdventOfCode2024.py
@@ -0,0 +1,118 @@
+'''
+Created on 2024-11-30
+
+@author: Sestren
+'''
+import argparse
+
+def get_raw_input_lines() -> list:
+    raw_input_lines = []
+    while True:
+        try:
+            raw_input_line = input()
+            raw_input_lines.append(raw_input_line)
+        except EOFError:
+            break
+        except StopIteration:
+            break
+        except KeyboardInterrupt:
+            break
+    return raw_input_lines
+
+class Template: # Template
+    '''
+    https://adventofcode.com/2024/day/?
+    '''
+    def get_parsed_input(self, raw_input_lines: list[str]):
+        result = []
+        for raw_input_line in raw_input_lines:
+            result.append(raw_input_line)
+        return result
+    
+    def solve(self, parsed_input):
+        result = len(parsed_input)
+        return result
+    
+    def solve2(self, parsed_input):
+        result = len(parsed_input)
+        return result
+    
+    def main(self):
+        raw_input_lines = get_raw_input_lines()
+        parsed_input = self.get_parsed_input(raw_input_lines)
+        solutions = (
+            self.solve(parsed_input),
+            self.solve2(parsed_input),
+            )
+        result = solutions
+        return result
+
+class Day01: # XXX
+    '''
+    https://adventofcode.com/2024/day/1
+    '''
+    def get_parsed_input(self, raw_input_lines: list[str]):
+        result = []
+        for raw_input_line in raw_input_lines:
+            result.append(raw_input_line)
+        return result
+    
+    def solve(self, parsed_input):
+        result = len(parsed_input)
+        return result
+    
+    def solve2(self, parsed_input):
+        result = len(parsed_input)
+        return result
+    
+    def main(self):
+        raw_input_lines = get_raw_input_lines()
+        parsed_input = self.get_parsed_input(raw_input_lines)
+        solutions = (
+            self.solve(parsed_input),
+            self.solve2(parsed_input),
+            )
+        result = solutions
+        return result
+
+if __name__ == '__main__':
+    '''
+    Usage
+    python AdventOfCode2024.py 1 < inputs/2024day01.in
+    '''
+    solvers = {
+        1: (Day01, 'XXX'),
+    #     2: (Day02, 'XXX'),
+    #     3: (Day03, 'XXX'),
+    #     4: (Day04, 'XXX'),
+    #     5: (Day05, 'XXX'),
+    #     6: (Day06, 'XXX'),
+    #     7: (Day07, 'XXX'),
+    #     8: (Day08, 'XXX'),
+    #     9: (Day09, 'XXX'),
+    #    10: (Day10, 'XXX'),
+    #    11: (Day11, 'XXX'),
+    #    12: (Day12, 'XXX'),
+    #    13: (Day13, 'XXX'),
+    #    14: (Day14, 'XXX'),
+    #    15: (Day15, 'XXX'),
+    #    16: (Day16, 'XXX'),
+    #    17: (Day17, 'XXX'),
+    #    18: (Day18, 'XXX'),
+    #    19: (Day19, 'XXX'),
+    #    20: (Day20, 'XXX'),
+    #    21: (Day21, 'XXX'),
+    #    22: (Day22, 'XXX'),
+    #    23: (Day23, 'XXX'),
+    #    24: (Day24, 'XXX'),
+    #    25: (Day25, 'XXX'),
+        }
+    parser = argparse.ArgumentParser()
+    parser.add_argument('day', help='Solve for a given day', type=int)
+    args = parser.parse_args()
+    day = args.day
+    solver = solvers[day][0]()
+    solutions = solver.main()
+    print(f'Solutions for Day {day}:', solvers[day][1])
+    print(f'  Part 1:', solutions[0])
+    print(f'  Part 2:', solutions[1])
